f3f7c6c3cbc7404354fa43dbe9dbeebfb2fdf6c2
Paresh Sahoo <85180102+pareshkumarsahoo@users.noreply.github.com>
2021-11-12 11:56:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 // cors middleware
 app.use(cors());
+const corsConfig = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+};
+app.use(corsConfig);
+
+app.options('*', cors());
 const Routes = require("./routes/Routes");
 const db = require("./config/keys");
 mongoose
