39f4721b6c4396f0d10aaa5e7ebe848f5cbb73e3
pareshkumarsahoo <paresh.sahoo@invincix.com>
2021-11-12 12:43:36 +0530

    cors

--- server.js
+++ server.js
@@ -11,16 +11,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 // cors middleware
-const corsConfig = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-};
-app.use(corsConfig);
+// const corsConfig = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     next();
+// };
+// app.use(corsConfig);
 
-app.options('*', cors());
+// app.options('*', cors());
+
+app.use(cors());
 const Routes = require("./routes/Routes");
 const db = require("./config/keys");
 mongoose
@@ -33,7 +35,7 @@ mongoose
   .then(() => {
     console.log("connection successfully ...");
   })
-  .catch((err) => console.log("Internal server Error"+err));
+  .catch((err) => console.log("Internal server Error" + err));
 
 app.use("/api", Routes);
 
